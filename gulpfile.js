f56738764a442f45955a928076481a3ac3080e8a
Yuki Sugawara <65204357+sgwr-0113@users.noreply.github.com>
2022-05-24 16:01:52 +0900

    set up compile environment

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const notify = require('gulp-notify');  // エラー通知
+const plumber = require('gulp-plumber'); // エラー時のタスク停止防止
+const debug = require('gulp-debug'); // ログ表示
+const dartSass = require('gulp-dart-sass');
+const autoprefixer = require('gulp-autoprefixer'); // ベンダープレフィックス付与
+const sourcemaps = require('gulp-sourcemaps'); // ソースマップ出力
+
+const paths = {
+  scss: {
+    src: 'src/assets/scss/**/*.scss', // コンパイル対象
+    dest: 'public/assets/css' // 出力先
+  }
+}
+
+/**
+ * scssタスクで実行する関数
+ */
+function scss() {
+  return gulp.src(paths.scss.src)
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(dartSass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer({
+      cascade: true
+    }))
+    .pipe(sourcemaps.write('/maps'))
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(debug({title: 'scss dest:'}));
+}
+
+exports.scss = scss; // scssタスク
+exports.default = gulp.series(scss); // defaultタスク
